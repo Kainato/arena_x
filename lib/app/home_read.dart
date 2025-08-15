ed68648b6a5e9199c53c1bda7ba80149ca9886ee
Kainato <caiocaladaraujo@gmail.com>
2025-08-15 09:23:36 -0300

    Aprimorar a tipagem das variáveis no método build da classe HomeRead

--- lib/app/home_read.dart
+++ lib/app/home_read.dart
@@ -5,14 +5,15 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../core/state/game_state.dart';
+import '../core/models/player.dart';
 
 class HomeRead extends StatelessWidget {
   const HomeRead({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final game = context.watch<GameState>();
-    final p = game.player;
+    final GameState game = context.watch<GameState>();
+    final Player p = game.player;
 
     return Scaffold(
       appBar: AppBar(title: const Text('EPIC-like RPG')),
